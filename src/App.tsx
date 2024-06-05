274013cebea73abd25a51db436cb648982846809
Eduard <eduard@solteca.cl>
2024-06-05 14:31:26 +0300

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Card from "./components/Card";
-import CardDefault from "./components/CardDefault";
 import Envelope from "./components/Envelope";
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      {/* <Card /> */}
-      {/* <CardDefault /> */}
       <Envelope />
     </React.Fragment>
   );
